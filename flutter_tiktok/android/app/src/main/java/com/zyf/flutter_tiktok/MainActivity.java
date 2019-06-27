31aeeea8c937a2a6fc69cd273cc6f14e92241140
zhangyufei <zhangyufei@xiaomi.com>
2019-06-27 14:10:59 +0800

    first commit

---
+++ flutter_tiktok/android/app/src/main/java/com/zyf/flutter_tiktok/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zyf.flutter_tiktok;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
